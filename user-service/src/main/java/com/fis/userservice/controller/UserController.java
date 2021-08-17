8116e3afb10445efcaf863e86e4c7a6d8af78e0b
Aish625 <aishwaraya625@gmail.com>
2021-08-17 14:03:02 +0530

    user service

--- user-service/src/main/java/com/fis/userservice/controller/UserController.java
+++ user-service/src/main/java/com/fis/userservice/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.fis.userservice.controller;
 
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,27 +8,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fis.userservice.bean.User;
-//import com.fis.springlearn.repository.UserRepository;
-//import com.fis.springlearn.service.AppUserDetailsService;
+import com.fis.userservice.repository.UserRepository;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-	//@Autowired
-	//private AppUserDetailsService appUserDetailsService;
+	/*
+	 * @Autowired private AppUserDetailsService appUserDetailsService;
+	 * 
+	 * @GetMapping("/{username}") public UserDetails findByUsername(@PathVariable
+	 * String username) { return appUserDetailsService.loadUserByUsername(username);
+	 * }
+	 */
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@GetMapping("/{username}")
-	public User getUser(@PathVariable String username) {
-		return new User(0, username);
+	public User findByUsername(@PathVariable String username) {
+		return userRepository.findByUsername(username);
 	}
 
-//	@Autowired
-//	private UserRepository userRepository;
-
-//	@GetMapping("/{username}")
-//	public User findByUsername(@PathVariable String username) {
-//		return userRepository.findByUsername(username);
 //	}
 
 }
