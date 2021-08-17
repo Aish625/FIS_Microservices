8dea2fd65d96dd3e1b9591c7fe03e2e77d43ee38
Aish625 <aishwaraya625@gmail.com>
2021-08-17 14:26:32 +0530

    user service

--- user-service/src/main/java/com/fis/userservice/controller/UserController.java
+++ user-service/src/main/java/com/fis/userservice/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.fis.userservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
